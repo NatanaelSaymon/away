deb5949a5e7e9743e6296f345030958d5e7b3c6d
Natanael <nsaymonsouza@gmail.com>
2020-10-22 16:10:58 -0300

    section works - mobile finalizado

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// const carouselPatrocinio = document.querySelector(".js-carousel")
+
+// new Glider(carouselPatrocinio, {
+//     slidesToShow: 6,
+//     slidToScroll: 1,
+//     draggable: true,
+//     scrollLock: true,
+// })
