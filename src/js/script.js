a1fa576e73883b452da2c1c351f74db0d8cc36ad
Natanael <nsaymonsouza@gmail.com>
2020-10-26 12:59:55 -0300

    Retirando gliderjs do projeto

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-// const carouselPatrocinio = document.querySelector(".js-carousel")
-
-// new Glider(carouselPatrocinio, {
-//     slidesToShow: 6,
-//     slidToScroll: 1,
-//     draggable: true,
-//     scrollLock: true,
-// })
