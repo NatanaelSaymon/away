6f99d3463b50d44bb8b8aae1c5c98446d3fa9c91
Natanael <nsaymonsouza@gmail.com>
2020-10-26 16:48:19 -0300

    Tiny Slider inserido no projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,36 @@
-const menuOpener = document.querySelector('.header__menu-opener')
-const nav = document.querySelector('.header__menu-nav')
+const menuOpener = document.querySelector(".header__menu-opener");
+const nav = document.querySelector(".header__menu-nav");
 
-menuOpener.addEventListener("click", function() {
-    nav.classList.toggle('active-menu')
-})
+menuOpener.addEventListener("click", function () {
+  nav.classList.toggle("active-menu");
+});
+
+var slider = tns({
+  container: ".patrocinio__list",
+  controls: false,
+  autoplayButtonOutput: false,
+  items: 2,
+  speed: 300,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  responsive: {
+
+    500: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2,
+    },
+
+    600: {
+        items: 3,
+    },
+
+    700: {
+        items: 4,       
+    },
+
+    900: {
+        items: 6,
+    },
+  },
+});
